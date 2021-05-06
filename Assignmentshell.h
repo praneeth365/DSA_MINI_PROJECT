395c73c076d47df480473c6e0c7ae8aa2885dab2
praneeth365 <78950227+praneeth365@users.noreply.github.com>
2021-05-06 14:23:34 +0530

    Update Assignmentshell.h

--- Assignmentshell.h
+++ Assignmentshell.h
@@ -1,1 +1,21 @@
-Assignmentshell.h
+#ifndef DSA_PROJECT
+#define DSA_PROJECT
+
+void Switch(char subject[100]) ;
+
+void CreateAssignment(char assignment[100]) ;
+
+void UpdateAssignment(char assignment[100]) ;
+
+void TestAssignment(char assignment[100]) ;
+
+void SubmitAssignment(char assignment[100]) ;
+
+void CompareAssignment(char assignment[100]) ;
+
+void UseAssignment(char assignment[100]) ;
+
+ 
+
+
+#endif
