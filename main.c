33e97a8d1945c1a57c529551a0ee7defa71bb9cb
praneeth365 <78950227+praneeth365@users.noreply.github.com>
2021-05-08 17:41:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,58 +1,74 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<unistd.h>
-#include<string.h>
- 
-int main(void)
+int main()
 {
     char string[100];
     char path[100];
-    //char command[100];
-    char argument[100];
+    char *argument;
+    char *command;
     int i = 0;
+    char assignment[100];
+    char pathtofolder[200];
+    memset(pathtofolder, '\0', sizeof(pathtofolder));
+    getcwd(pathtofolder, 200);
+    int t=0 ;
     while (1)
     {
-        printf("TEAM53:");
-        printf("%s>>>", getcwd(path, 100));
- 
-        fgets(string,100, stdin);
-        char * command = strtok(string, " ");
-        char * argument = strtok(NULL, " ");
-        char* defaultargument[100]="\0" ;
-     
-         if(argument==NULL)
-           command[strlen(command)-1]="\0" ;
-
-        if(strcmp(command,"use")==0&&argument!=NULL)
-            strcpy(defaultargument,argument) 
- 
-        if (strcmp(command, "exit") == 0)
+        printf("\nteam53");
+        printf("%s >", getcwd(path, 100));
+        memset(string, '\0', sizeof(string));
+        fgets(string, 200, stdin);
+        command = strtok(string, " "); //spliting the input into command
+        argument = strtok(NULL, " ");  //splitting the input into argument
+        if(argument!=NULL)
+        argument[strlen(argument)-1]='\0';
+        if (strcmp(command, "exit\n") == 0)
             break;
- 
-        if (strcmp(command, "switch") == 0)
-            Switch(argument);
- 
-        else if (strcmp(command, "create") == 0)
-            CreateAssignment(argument\n);
- 
-        else if (strcmp(command, "update") == 0)
-            UpdateAssignment(argument);
- 
-        else if (strcmp(command, "test") == 0)
-            TestAssignment(argument);
- 
-        else if (strcmp(command, "submit") == 0)
-            SubmitAssignment(argument);
- 
-        else if (strcmp(command, "compare") == 0)
-            CompareAssignment(argument);
- 
-        else if (strcmp(command, "use") == 0)
-        {
-            UseAssignment(defaultargument);
+        if ((!argument) && t) //this is find wheather there is an argument or not(if argument=NULL then if(!argument)is true)
+        {                     // based on the command we call the relevent function
+            if (strcmp(string, "create\n") == 0)
+                CreateAssignment(assignment);
+
+            else if (strcmp(string, "update\n") == 0)
+                UpdateAssignment(assignment);
+
+            else if (strcmp(string, "test\n") == 0)
+               TestAssignment(assignment);
+
+            else if (strcmp(string, "submit\n") == 0)
+               SubmitAssignment(assignment);
+
+            else if (strcmp(string, "compare\n") == 0)
+               CompareAssignment(assignment);
         }
- 
         else
-            printf("ERROR!\n");
+        {
+            if (strcmp(command, "switch") == 0)
+              Switch(pathtofolder, argument);
+
+            else if (strcmp(command, "create") == 0)
+                CreateAssignment(argument);
+
+            else if (strcmp(command, "update") == 0)
+               UpdateAssignment(argument);
+
+            else if (strcmp(command, "test") == 0)
+                 TestAssignment(argument);
+
+            else if (strcmp(command, "submit") == 0)
+             SubmitAssignment(argument);
+
+            else if (strcmp(command, "compare") == 0)
+                 CompareAssignment(argument);
+
+            else if (strcmp(command, "use") == 0)
+            {
+                memset(assignment, '\0', sizeof(assignment));
+                strcpy(assignment, argument);
+                t = 1;
+            }
+
+            else
+                printf("ERROR!");
+        }
     }
+    return 0;
 }
