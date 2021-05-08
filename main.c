a4d2584372713b1358b18ded879337c252a47a53
praneeth365 <78950227+praneeth365@users.noreply.github.com>
2021-05-08 18:32:23 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include "Assignmentshell.c"
+
+
+
 int main()
 {
     char string[100];
