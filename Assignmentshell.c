a069cf435a5f814374460cfffa52fabe5d081615
sehgaladitya26 <80144935+sehgaladitya26@users.noreply.github.com>
2021-05-08 15:39:18 +0530

    Update Assignmentshell.c

--- Assignmentshell.c
+++ Assignmentshell.c
@@ -59,7 +59,7 @@ int createAssignment(char assignment[10000])
 void CopyFileFunction(char constdir[10000])
 {
     char movFunction[10000] = "mv -v ";
-    char src[10000] = "/home/welcome/DSA_MINI_PROJECT/dist ";
+    char src[10000] = "/home/welcome/DSA_MINI_PROJECT/dist/ ";
 
     strcat(src, constdir);
     strcat(movFunction, src);
